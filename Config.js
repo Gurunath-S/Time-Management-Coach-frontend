73a79d7d1c947df9fff50ba1965d136c0314b556
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-10-06 19:03:50 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,8 @@
+// const BACKEND_URL = "https://time-management-coach-backend.onrender.com";
+// https://time-management-coach-testing.onrender.com 
+// const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+// console.log("BACKEND_URL:", BACKEND_URL);
+
+const BACKEND_URL ="https://time-management-coach-backend.vercel.app"
+// const BACKEND_URL ="http://localhost:5000";
+export default BACKEND_URL;
