3e797b4469f5a5620fb25469cee71879bc635008
Guru <139735814+Gurunath-S@users.noreply.github.com>
2026-02-12 12:54:35 +0530

    Add Google Analytics initialization
    Initialize Google Analytics if GA ID is present.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import ReactGA from "react-ga4";
+
+if (import.meta.env.VITE_GA_ID) {
+  ReactGA.initialize(import.meta.env.VITE_GA_ID);
+}
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
